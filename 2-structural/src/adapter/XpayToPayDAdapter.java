ac945ff829fda9f17db7b32f29215ffb59e05019
MimiTaylor <118178517+MimiTaylor@users.noreply.github.com>
2026-02-20 19:14:57 +0000

    completed adapter task

--- 2-structural/src/adapter/XpayToPayDAdapter.java
+++ 2-structural/src/adapter/XpayToPayDAdapter.java
@@ -1,62 +1,66 @@
 package adapter;
 
 public class XpayToPayDAdapter implements PayD {
+
+    // using composition 'HAS-A' Xpay object.
+    // it is referencing any object that implements Xpay interface
+    // xpayObject name might be a bit misleading but I get it. (obvs you cannot instantiate an interface)
+    Xpay xpayObject;
+
     public XpayToPayDAdapter(Xpay xpay) {
-        // TODO: implement
+        this.xpayObject = xpay;
     }
+
 
     @Override
     public String getCustCardNo() {
-        // TODO: implement
-        return null;
+        return xpayObject.getCreditCardNo();
     }
 
     @Override
     public String getCardOwnerName() {
-        // TODO: implement
-        return null;
+        return xpayObject.getCustomerName();
     }
 
     @Override
     public String getCardExpMonthDate() {
-        // TODO: implement
-        return null;
+        return xpayObject.getCardExpMonth();
     }
 
     @Override
     public int getCVVNo() {
-        // TODO: implement
-        return 0;
+        return xpayObject.getCardCVVNo();
     }
 
     @Override
     public double getTotalAmount() {
-        // TODO: implement
-        return 0;
+        return xpayObject.getAmount();
     }
 
     @Override
     public void setCustCardNo(String custCardNo) {
-        // TODO: implement
+        xpayObject.setCreditCardNo(custCardNo);
     }
 
     @Override
     public void setCardOwnerName(String cardOwnerName) {
-        // TODO: implement
+        xpayObject.setCustomerName(cardOwnerName);
     }
 
     @Override
     public void setCardExpMonthDate(String cardExpMonthDate) {
-        // TODO: implement
+        xpayObject.setCardExpMonth(cardExpMonthDate);
     }
 
     @Override
     public void setCVVNo(int cVVNo) {
-        // TODO: implement
+        xpayObject.setCardCVVNo((short) cVVNo);
     }
 
     @Override
     public void setTotalAmount(double totalAmount) {
-        // TODO: implement
-    }
+        xpayObject.setAmount(totalAmount);    }
 }
+
+// derek banas - adapter design pattern
+// https://www.youtube.com/watch?v=qG286LQM6BU&t=734s
